7137de6a8c05ae045e545d650edda4cc2c2f08d0
mauriciobrito7 <maubot01@gmail.com>
2020-04-12 01:35:35 -0400

    creating sing in form

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import { Route, Switch } from "react-router-dom";
 // Pages
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import signInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+
+// Components
+import Header from "./components/header/header.component";
 
 import "./App.css";
 
@@ -11,9 +15,11 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/shop" component={ShopPage} />
+          <Route exact path="/signIn" component={signInAndSignUpPage} />
         </Switch>
       </div>
     );
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import SignIn from "../../components/sign-in/sign-in.component";
+
+// Styles
+import "./sign-in-and-sign-up.styles.scss";
+
+const signInAndSignUpPage = () => {
+  return (
+    <div className="sign-in-and-sign-up">
+      <SignIn />
+    </div>
+  );
+};
+
+export default signInAndSignUpPage;
