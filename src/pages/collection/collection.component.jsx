cd060abc0d68da5d4b1c43cb3c241ff35c2c060b
mauriciobrito7 <maubot01@gmail.com>
2020-04-24 23:34:59 -0400

    fix don't render collection page

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,23 +1,20 @@
 import React from "react";
-// Components
+import { connect } from "react-redux";
+
 import CollectionItem from "../../components/collection-item/collection-item.component";
 
-// Redux
-import { connect } from "react-redux";
 import { selectCollection } from "../../redux/shop/shop.selectors";
 
-// Styles
 import "./collection.styles.scss";
 
 const CollectionPage = ({ collection }) => {
   const { title, items } = collection;
   return (
     <div className="collection-page">
-      <h2>{title}</h2>
-      <h1 className="title">Hola Mundo</h1>
+      <h2 className="title">{title}</h2>
       <div className="items">
         {items.map((item) => (
-          <CollectionItem key={items.id} item={item} />
+          <CollectionItem key={item.id} item={item} />
         ))}
       </div>
     </div>
