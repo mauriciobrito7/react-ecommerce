cf6e9f382157786a039029f5b1dbdf7021df95ab
Mauricio <maubot01@gmail.com>
2021-02-27 13:39:44 -0400

    theme and breakpoints

---
+++ src/theme.js
@@ -0,0 +1,40 @@
+export const breakpoints = {
+  mobile: "375px",
+  tablet: "768px",
+  desktop: "1024px",
+  large: "1400px",
+};
+
+export const device = {
+  mobile: (styles) => {
+    return `@media (min-width:${breakpoints.mobile}){
+      ${styles}
+    }`;
+  },
+  tablet: (styles) => {
+    return `@media (min-width:${breakpoints.tablet}){
+      ${styles}
+    }`;
+  },
+  desktop: (styles) => {
+    return `@media (min-width:${breakpoints.desktop}){
+      ${styles}
+    }`;
+  },
+  large: (styles) => {
+    return `@media (min-width:${breakpoints.large}){
+      ${styles}
+    }`;
+  },
+  custom: (styles, width) => {
+    return `@media (min-width:${width}){
+      ${styles}
+    }`;
+  },
+};
+
+export const colors = {
+  black: "#000000",
+  white: "#ffffff",
+  primary: "rgba(23,26,32,0.8)",
+};
