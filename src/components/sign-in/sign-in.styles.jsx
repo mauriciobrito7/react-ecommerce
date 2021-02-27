4494fa4a3ba85cc7085cc3b9f868bc3ef91fee0d
Mauricio <maubot01@gmail.com>
2021-02-27 14:07:23 -0400

    responsive styles

--- src/components/sign-in/sign-in.styles.jsx
+++ src/components/sign-in/sign-in.styles.jsx
@@ -1,16 +1,30 @@
 import styled from "styled-components";
+import { device } from "../../theme";
 
 export const SignInContainer = styled.div`
-  width: 380px;
+  width: 100%;
   display: flex;
   flex-direction: column;
+  ${device.mobile`
+   width:70%; 
+  `}
+  ${device.tablet`
+    width:60%; 
+  `}
+  ${device.desktop`
+    width:45%; 
+  `}
+  ${device.large`
+    width:40%; 
+  `}
 `;
 
 export const SignInTitle = styled.h2`
-  margin: 10px 0;
+  margin: 0.5em 0;
 `;
 
 export const ButtonsBarContainer = styled.div`
   display: flex;
+  flex-wrap: wrap;
   justify-content: space-between;
 `;
--- src/components/sign-up/sign-up.styles.jsx
+++ src/components/sign-up/sign-up.styles.jsx
@@ -1,11 +1,24 @@
 import styled from "styled-components";
+import { device } from "../../theme";
 
 export const SignUpContainer = styled.div`
   display: flex;
   flex-direction: column;
-  width: 380px;
+  width: 100%;
+  ${device.mobile`
+   width:70%; 
+  `}
+  ${device.tablet`
+    width:60%; 
+  `}
+  ${device.desktop`
+    width:45%; 
+  `}
+   ${device.large`
+    width:40%; 
+  `}
 `;
 
 export const TitleContainer = styled.h2`
-  margin: 10px 0;
+  margin: 0.5em 0;
 `;
