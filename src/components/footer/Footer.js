bc8a92e74c70e28af94285278c841749dd687561
Mauricio <maubot01@gmail.com>
2021-02-27 18:26:46 -0400

    footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,10 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
 import React from "react";
 import { ContainerFlex } from "./Footer.styles";
 const Footer = () => {
   return (
     <ContainerFlex>
-      Proudly made in 🇻🇪 by &nbsp;
+      Proudly made in <span>🇻🇪</span> by &nbsp;
       <a
         href="https://www.linkedin.com/in/mauricio-brito-62b0a6140/"
         target="blind"
